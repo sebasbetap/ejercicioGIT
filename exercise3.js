14c86279519bae2afd8f34f7c262475d6157f5d0
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-01-05 14:55:24 -0500

    Excercises-january

--- exercise3.js
+++ exercise3.js
@@ -1,10 +1,10 @@
-var chosenNumber = 8; // Ingresa el número que se desea verificar si es par o impar
+var chosenNumber = [4,3,5,6,7,8];
 
 isOdd (chosenNumber);
 
 function isOdd(value) {
     if (value%2 == 0) {
-        alert("El número ingresado es Par");
+        
         console.log("El número ingresado es Par");
     } else {
         alert("El número ingresado es Impar");
