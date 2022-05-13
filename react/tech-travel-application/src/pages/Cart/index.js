6b4530c4cf2c012d5e14a4b3a4d22e92446519f6
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-05-13 09:53:33 -0500

    React-project-completed

--- react/tech-travel-application/src/pages/Cart/index.js
+++ react/tech-travel-application/src/pages/Cart/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MdDelete } from 'react-icons/md';
+import CartContext from '../../context/cart';
+import {
+  Container,
+  ContainerList,
+  TravelItem,
+  Info,
+  Quantity,
+  Subtotal,
+  Plink,
+} from './styles';
 
 function Cart() {
-  return <h1>Cart</h1>;
+  const { state, setState } = useContext(CartContext);
+
+  return (
+    <Container>
+      <ContainerList>
+        {state.cart.map((el) => (
+          <TravelItem>
+            <img src={el.photo} alt={el.title} />
+            <Info>
+              <p>{el.title}</p>
+              <strong>{el.price}</strong>
+            </Info>
+            <Quantity readOnly type="number" value={el.quantity} />
+            <Subtotal>
+              <p>{el.quantity * el.price}</p>
+              <button type="button">
+                <MdDelete size={24} color="#0676d9" />
+              </button>
+            </Subtotal>
+          </TravelItem>
+        ))}
+      </ContainerList>
+    </Container>
+  );
 }
 
 export default Cart;
